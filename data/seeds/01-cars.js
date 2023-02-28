0612ebcb71379abde0609d4dba510ee2ddd57e7f
Owen McComas <owen@mccomas.me>
2023-02-28 10:09:26 -0700

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin: '4709528794724',
+        make: 'toyota',
+        model: 'prius',
+        milage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '4709528794724',
+        make: 'toyota',
+        model: 'prius',
+        milage: 115000,
+        title: 'salvage',
+    },
+    {
+        vin: '4709528794724',
+        make: 'ford',
+        model: 'focus',
+        milage: 15000,
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
